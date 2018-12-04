eda0af5c611993f2d2ed2bb49f795f18b62558c3
Кирилл Штеффен <kirillshteffen@gmail.com>
2018-12-04 10:41:33 +0600

    Finished Framework

---
+++ FireHelper/FireHelper/FireHelper.h
@@ -0,0 +1,19 @@
+//
+//  FireHelper.h
+//  FireHelper
+//
+//  Created by Kirill Shteffen on 04/12/2018.
+//  Copyright © 2018 BlackBricks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FireHelper.
+FOUNDATION_EXPORT double FireHelperVersionNumber;
+
+//! Project version string for FireHelper.
+FOUNDATION_EXPORT const unsigned char FireHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FireHelper/PublicHeader.h>
+
+
---
+++ FireHelper/Pods/Target Support Files/Pods-FireHelper/Pods-FireHelper-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FireHelper : NSObject
+@end
+@implementation PodsDummy_Pods_FireHelper
+@end
